074d15eff2c32456aad4c7257c0886635cb0b37c
ibrahim11elian <ibrahim11elian@gmail.com>
2023-12-15 01:51:28 +0200

    update app.js after using context hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import ScrollToTop from "react-scroll-to-top";
 import Biography from "./components/biography";
 import ContactMe from "./components/contact_me";
@@ -7,49 +7,17 @@ import Header from "./components/header";
 import Home from "./components/home";
 import Portfolio from "./components/portfolio";
 import Skills from "./components/skills";
+import { useAppContext } from "./utilities/context";
 
 function App() {
-  const [active, setActive] = useState("");
-  const toggleMenu = () => {
-    setActive(() => (active === "active" ? "" : "active"));
-    document.body.classList.toggle("active");
-    document.querySelector(".menu-icon").classList.toggle("fa-xmark");
-  };
-
-  window.onscroll = () => {
-    if (active === "active") {
-      if (window.innerWidth < 991) {
-        setActive(() => "");
-        document.body.classList.remove("active");
-        document.querySelector(".menu-icon").classList.remove("fa-xmark");
-      }
-
-      document.querySelectorAll("section").forEach((sec) => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute("id");
-
-        if (top >= offset && top < offset + height) {
-          document
-            .querySelectorAll(".nav-container ul li a")
-            .forEach((links) => {
-              links.classList.remove("active");
-              document
-                .querySelector(".nav-container ul li a[href*=" + id + "]")
-                .classList.add("active");
-            });
-        }
-      });
-    }
-  };
+  const { theme, active } = useAppContext();
 
   const year = new Date().getFullYear();
 
   return (
-    <>
+    <div className={`app ${active ? "active" : ""}`} theme={theme}>
       <Home />
-      <Header toggleMenu={toggleMenu} active={active} />
+      <Header />
       <Biography />
       <Skills />
       <Education />
@@ -63,7 +31,7 @@ function App() {
         top={700}
         component={<i className="up-icon fa-sharp fa-solid fa-angle-up" />}
       />
-    </>
+    </div>
   );
 }
 
