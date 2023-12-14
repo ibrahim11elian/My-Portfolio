8fedc189ab6382c5afa75053460d6c4cbebedd8b
ibrahim11elian <ibrahim11elian@gmail.com>
2023-12-15 01:58:28 +0200

    skills data was made for holding the techs data

---
+++ src/data/skills_data.js
@@ -0,0 +1,18 @@
+export const skills = {
+  HTML5: "html5/html5-original.svg",
+  CSS3: "css3/css3-original.svg",
+  JavaScript: "javascript/javascript-original.svg",
+  Typescript: "typescript/typescript-original.svg",
+  ReactJS: "react/react-original.svg",
+  Nodejs: "nodejs/nodejs-original.svg",
+  Expressjs: "express/express-original.svg",
+  PostgreSQL: "postgresql/postgresql-original.svg",
+  Jasmine: "jasmine/jasmine-plain.svg",
+  Bootstrap: "bootstrap/bootstrap-original.svg",
+  Sass: "sass/sass-original.svg",
+  Git: "git/git-original.svg",
+  Github: "github/github-original.svg",
+  vscode: "vscode/vscode-original.svg",
+  C: "c/c-original.svg",
+  Python: "python/python-original.svg",
+};
