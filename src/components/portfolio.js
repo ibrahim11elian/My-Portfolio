0576a6e71bda5ba1094cdd8bdd9f74eb41886e77
ibrahim11elian <ibrahim11elian@gmail.com>
2024-07-09 16:14:52 +0300

    add active class to style the active category && update the project card layout and style

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,5 +1,6 @@
 /* eslint-disable react/jsx-no-target-blank */
 import React, { useState } from "react";
+import { FiGithub, FiGlobe } from "react-icons/fi";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import { data } from "../data/projects_data";
 const backImage = "./images/back.svg";
@@ -8,20 +9,25 @@ const backImage = "./images/back.svg";
 function Portfolio() {
   // State to manage the list of projects
   const [projects, setProjects] = useState([...data]);
+  const [active, setActive] = useState("all");
 
   // Function to filter projects based on the category button clicked
-  const filter = (e) => {
-    const btn = e.target.textContent.toLowerCase().split("-")[0];
+  const filter = (element) => {
+    const category = element.toLowerCase().split("-")[0];
 
-    if (btn === "all") {
+    if (category === "all") {
       setProjects([...data]);
+      setActive("all");
     } else {
       const newList = data.filter((e) => {
-        return e.category === btn;
+        return e.category === category;
       });
       setProjects([...newList]);
+      setActive(category);
     }
   };
+
+  const filterList = ["all", "front-end", "back-end", "full-stack"];
 
   // Render the Portfolio section
   return (
@@ -32,38 +38,25 @@ function Portfolio() {
       </h1>
 
       {/* Filter buttons */}
-      <ul className="filter">
-        <li>
-          <button className="btn" data-aos="fade-up" onClick={(e) => filter(e)}>
-            all
-          </button>
-        </li>
-        <li>
-          <button className="btn" data-aos="fade-up" onClick={(e) => filter(e)}>
-            Front-End
-          </button>
-        </li>
-        <li>
-          <button className="btn" data-aos="fade-up" onClick={(e) => filter(e)}>
-            Back-End
-          </button>
-        </li>
-        <li>
-          <button className="btn" data-aos="fade-up" onClick={(e) => filter(e)}>
-            Full-Stack
-          </button>
-        </li>
+      <ul className="filter" data-aos="fade-up">
+        {filterList.map((ele) => {
+          return (
+            <li key={ele}>
+              <button
+                className={`btn ${ele.startsWith(active) ? "active" : ""}`}
+                onClick={() => filter(ele)}
+              >
+                {ele}
+              </button>
+            </li>
+          );
+        })}
       </ul>
 
       {/* Project list */}
       <ul className="project-list">
         {projects.map((ele) => {
-          return (
-            <li key={ele.id}>
-              {/* Project component */}
-              <Project project={ele} />
-            </li>
-          );
+          return <Project project={ele} key={ele.id} />;
         })}
       </ul>
     </section>
@@ -72,39 +65,44 @@ function Portfolio() {
 
 // Project component
 function Project({ project }) {
-  // Render individual project
   return (
-    <div className="project" data-aos="zoom-in">
-      {/* Project image */}
-      <LazyLoadImage
-        srcSet={project.img || backImage}
-        alt={project.name}
-        className="project-img"
-        effect="blur" // Apply a blur effect during lazy loading
-      />
-      {/* Project information */}
-      <div className="project-info">
-        <div className="name">{project.name}</div>
-        {/* Project tags */}
-        <div className="tags">
-          {project.tags.map((e, i) => {
-            return <span className="tag" key={i}>{`#${e}`}</span>;
-          })}
+    <li className="card">
+      <a className="card-img-link" href={project.link} target="_blank">
+        <LazyLoadImage
+          src={project.img || backImage}
+          alt={project.name}
+          effect="blur"
+          title={project.name}
+          className="card-img"
+        />
+      </a>
+      <div className="card-content">
+        <div className="card-text">
+          <h3 className="card-title">{project.name}</h3>
+          <ul className="card-tags">
+            {project.tags?.slice(0, 4).map((tag) => {
+              return (
+                <li className="tag" key={tag}>
+                  {tag}
+                </li>
+              );
+            })}
+          </ul>
         </div>
-        {/* Project links */}
-        <div className="links">
-          {project.live ? (
-            <a href={project.live} className="btn" target={"_blank"}>
-              live site
+        <div className="card-buttons">
+          {project.live && (
+            <a href={project.live} className="live-site-button btn">
+              <FiGlobe className="live-icon" />
+              {project.category === "back" ? "Live API" : " Live Site"}
             </a>
-          ) : null}
-
-          <a href={project.link} className="btn" target={"_blank"}>
-            project link
+          )}
+          <a href={project.link} className="github-button btn">
+            <FiGithub className="git-icon" />
+            GitHub
           </a>
         </div>
       </div>
-    </div>
+    </li>
   );
 }
 
